a3a491608cd1f4a26cd1e3818a8344047182ac21
funster <vovanphuong101098@gmail.com>
2024-05-18 09:47:59 +0700

    feat: login

---
+++ coreui-free-react-admin-template/src/PrivateRoute.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const PrivateRoute = () => {
+  const isAuthenticated = useSelector((state) => state.isAuthenticated)
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+}
+
+export default PrivateRoute
--- coreui-free-react-admin-template/src/store.js
+++ coreui-free-react-admin-template/src/store.js
@@ -3,12 +3,17 @@ import { legacy_createStore as createStore } from 'redux'
 const initialState = {
   sidebarShow: true,
   theme: 'light',
+  isAuthenticated: false, // Add authentication state
 }
 
 const changeState = (state = initialState, { type, ...rest }) => {
   switch (type) {
     case 'set':
       return { ...state, ...rest }
+    case 'login':
+      return { ...state, isAuthenticated: true }
+    case 'logout':
+      return { ...state, isAuthenticated: false }
     default:
       return state
   }
